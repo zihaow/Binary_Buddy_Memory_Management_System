1119b50b336c3878a4ad6ee1caec2073f1aa7447
zihaow <zihao.wu1993@gmail.com>
2015-06-15 20:26:53 -0300

    Command line feature added
    Optional command line feature added for the memory management system.
    Users can build their own custom command line system as well.

--- test.c
+++ test.c
@@ -16,40 +16,61 @@ typedef struct {
 } test;
 
 int main(int argc, char **argv) {
-    /*
+    
     int startMemory = 0;
     int memorySize = 0;
-    
+    int count = 0;
+    char text[20];
+    char allo[5] = "allo", quit[5] = "quit";
+
     printf("Enter the memory size:");
-    
     scanf("%d",&startMemory);
     start_memory(startMemory);
     
     
     while(1){
-        printf("Enter the amount you want to be allocated:");
-        scanf("%d",&memorySize);
-        get_memory(memorySize);
+
+        /* to handle user command. */
+        fgets(text,sizeof text, stdin);
+        strtok(text,"\n");
+        if (strcmp(text, quit) == 0){
+            
+            if( count == 1){
+                exit(0);
+            }
+            
+            end_memory();
+            exit(0);
+        }
+        else if(strcmp(text, allo) == 0 ){
+            printf("Enter the amount you want to be allocated: ");
+            scanf("%d",&memorySize);
+            get_memory(memorySize);
+        }
+        else{
+            printf("Enter the command. 'allo' to allocate memory; 'quit' to quit the system.\n\n");
+        }
+        count++;
     }
-    */
     
-    // initialize memory size to 128.
-    ///*
+    
+    // initialize memory size to 512.
+    /*
     start_memory(512);
     printf("\n");
 
-    test *t1 = get_memory(4);
+    test *t1 = get_memory(8);
     printf("Memory address for t1 is %p\n", (void *)t1);
     printf("--------------------------------\n");
     
-    test *t2 = get_memory(4);
-    grow_memory(12, t2);
+    test *t2 = get_memory(8);
+    //grow_memory(12, t2);
     printf("Memory address for t2 is %p\n", (void *)t2);
     printf("--------------------------------\n");
     
     
     test *t3 = get_memory(8);
-    pregrow_memory(12, t2);
+    //pregrow_memory(12, t2);
     //test *t3 = get_memory((sizeof(test)));
     printf("Memory address for t3 is %p\n", (void *)t3);
     printf("--------------------------------\n");
@@ -58,17 +79,15 @@ int main(int argc, char **argv) {
     //grow_memory(12, t4);
     printf("Memory address for t4 is %p\n", (void *)t4);
     printf("--------------------------------\n");
-    //*/
-    
     
     //release_memory(t4);
-    /*
+    
     test *t5 = get_memory(8);
     //grow_memory(12, t2);
     printf("Memory address for t5 is %p\n", (void *)t5);
     printf("--------------------------------\n");
     
-    test *t6 = get_memory(64);
+    test *t6 = get_memory(8);
     //grow_memory(12, t2);
     printf("Memory address for t6 is %p\n", (void *)t6);
     printf("--------------------------------\n");
@@ -78,21 +97,14 @@ int main(int argc, char **argv) {
     printf("Memory address for t7 is %p\n", (void *)t7);
     printf("--------------------------------\n");
     
-    test *t8 = get_memory(64);
+    test *t8 = get_memory(8);
     //grow_memory(12, t2);
     printf("Memory address for t8 is %p\n", (void *)t8);
-    printf("--------------------------------\n");
-     */
-    
-    /*
-    test *t9 = get_memory(4);
-    //grow_memory(12, t2);
-    printf("Memory address for t9 is %p\n", (void *)t9);
     printf("--------------------------------\n");
     */
     
     /* to clean up all memory.*/
-    end_memory();
+    //end_memory();
     
     return 1;
 }
