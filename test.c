d1526198e2a23dab53125092d7c339caa86184f1
Zihao Wu <zihao.wu1993@gmail.com>
2015-06-15 16:44:10 -0300

    Update test.c

--- test.c
+++ test.c
@@ -16,6 +16,7 @@ typedef struct {
 } test;
 
 int main(int argc, char **argv) {
+    
     /*
     int startMemory = 0;
     int memorySize = 0;
@@ -25,7 +26,6 @@ int main(int argc, char **argv) {
     scanf("%d",&startMemory);
     start_memory(startMemory);
     
-    
     while(1){
         printf("Enter the amount you want to be allocated:");
         scanf("%d",&memorySize);
@@ -33,8 +33,7 @@ int main(int argc, char **argv) {
     }
     */
     
-    // initialize memory size to 128.
-    ///*
+    // initialize memory size to 512.
     start_memory(512);
     printf("\n");
 
@@ -47,49 +46,14 @@ int main(int argc, char **argv) {
     printf("Memory address for t2 is %p\n", (void *)t2);
     printf("--------------------------------\n");
     
-    
     test *t3 = get_memory(8);
     pregrow_memory(12, t2);
-    //test *t3 = get_memory((sizeof(test)));
     printf("Memory address for t3 is %p\n", (void *)t3);
     printf("--------------------------------\n");
     
     test *t4 = get_memory(8);
-    //grow_memory(12, t4);
     printf("Memory address for t4 is %p\n", (void *)t4);
     printf("--------------------------------\n");
-    //*/
-    
-    
-    //release_memory(t4);
-    /*
-    test *t5 = get_memory(8);
-    //grow_memory(12, t2);
-    printf("Memory address for t5 is %p\n", (void *)t5);
-    printf("--------------------------------\n");
-    
-    test *t6 = get_memory(64);
-    //grow_memory(12, t2);
-    printf("Memory address for t6 is %p\n", (void *)t6);
-    printf("--------------------------------\n");
-    
-    test *t7 = get_memory(8);
-    //grow_memory(12, t2);
-    printf("Memory address for t7 is %p\n", (void *)t7);
-    printf("--------------------------------\n");
-    
-    test *t8 = get_memory(64);
-    //grow_memory(12, t2);
-    printf("Memory address for t8 is %p\n", (void *)t8);
-    printf("--------------------------------\n");
-     */
-    
-    /*
-    test *t9 = get_memory(4);
-    //grow_memory(12, t2);
-    printf("Memory address for t9 is %p\n", (void *)t9);
-    printf("--------------------------------\n");
-    */
     
     /* to clean up all memory.*/
     end_memory();
